f04c4f6c5bc1920fbd202abe89042458a5c57e8a
miguel-siqueira-reis <miguel.siqueira.reis@gmail.com>
2022-07-28 02:11:26 -0300

    test: return error in body when no is name provided message erro body (Name is Required)

--- src/Presentation/Controllers/SignUpController.spec.ts
+++ src/Presentation/Controllers/SignUpController.spec.ts
@@ -1,17 +1,18 @@
 import { SignUpController } from './SignUpController';
 
 describe('SignUpController', () => {
-  it('should return 400 if no name is provided', function () {
-    const stu = new SignUpController();
-    const req = {
-      body: {
-        email: 'any_mail@mail.com',
-        password: 'any_password',
-      },
-    };
+    it('should return 400 if no name is provided', function () {
+        const stu = new SignUpController();
+        const req = {
+            body: {
+                email: 'any_mail@mail.com',
+                password: 'any_password',
+            },
+        };
 
-    const response = stu.handle(req);
+        const response = stu.handle(req);
 
-    expect(response.statusCode).toBe(400);
-  });
+        expect(response.statusCode).toBe(400);
+        expect(response.body).toEqual(new Error('Name is required'));
+    });
 });
