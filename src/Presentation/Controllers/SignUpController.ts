0120011cd049d0a5294e359501084ef8a60557fd
miguel-siqueira-reis <miguel.siqueira.reis@gmail.com>
2022-07-28 02:11:07 -0300

    feat: return error in body when no is name provided message erro body (Name is Required)

--- src/Presentation/Controllers/SignUpController.ts
+++ src/Presentation/Controllers/SignUpController.ts
@@ -2,6 +2,7 @@ export class SignUpController {
     public handle(req: any): any {
         return {
             statusCode: 400,
+            body: new Error('Name is required'),
         };
     }
 }
