ef80270b844f8a55a0254abcb4ca97356b3e062f
miguel-siqueira-reis <miguel.siqueira.reis@gmail.com>
2022-07-30 19:09:00 -0300

    test: ensure BcryptAdapter return hash on success

--- src/Infra/Criptography/BcryptAdapter.spec.ts
+++ src/Infra/Criptography/BcryptAdapter.spec.ts
@@ -1,6 +1,10 @@
 import bcrypt from 'bcrypt';
 import { BcryptAdapter } from './BcryptAdapter';
 
+jest.mock('bcrypt', () => ({
+    hash: (): Promise<string> => Promise.resolve('any_hash'),
+}));
+
 describe('BcryptAdapter', () => {
     it('should call bcrypt with correct params', async () => {
         const sut = new BcryptAdapter(12);
@@ -8,5 +12,12 @@ describe('BcryptAdapter', () => {
         await sut.encrypt('any_value');
 
         expect(hashSpy).toHaveBeenCalledWith('any_value', 12);
+    });
+
+    it('should returns hash on success', async () => {
+        const sut = new BcryptAdapter(12);
+        const hash = await sut.encrypt('any_value');
+
+        expect(hash).toBe('any_hash');
     });
 });
