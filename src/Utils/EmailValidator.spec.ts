8caa1b992c4ec129def1622b27196e6f4afc7487
miguel-siqueira-reis <miguel.siqueira.reis@gmail.com>
2022-07-29 22:52:24 -0300

    test: ensure EmailValidator returns true with validator returns true

--- src/Utils/EmailValidator.spec.ts
+++ src/Utils/EmailValidator.spec.ts
@@ -1,10 +1,25 @@
+// eslint-disable-next-line @typescript-eslint/no-unused-vars
+import validator from 'validator';
 import { EmailValidatorAdapter } from './EmailValidator';
 
+jest.mock('validator', () => ({
+    isEmail: jest.fn(() => true),
+}));
+
 describe('EmailValidator', () => {
-    it('should return false if validator returns false', function () {
+    it('should return false if validator returns false', () => {
         const sut = new EmailValidatorAdapter();
+
+        jest.spyOn(validator, 'isEmail').mockReturnValueOnce(false);
         const isValid = sut.isValid('invalid_mail@mail.com');
 
         expect(isValid).toBe(false);
+    });
+
+    it('should return true if validator returns true', () => {
+        const sut = new EmailValidatorAdapter();
+        const isValid = sut.isValid('valid_mail@mail.com');
+
+        expect(isValid).toBe(true);
     });
 });
