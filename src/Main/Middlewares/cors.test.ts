34fccde25b82d6f76ee8e69b71d8cc4b82273a31
miguel-siqueira-reis <miguel.siqueira.reis@gmail.com>
2022-07-31 13:18:58 -0300

    feat: cors enable

---
+++ src/Main/Middlewares/cors.test.ts
@@ -0,0 +1,17 @@
+// eslint-disable-next-line import/no-extraneous-dependencies
+import request from 'supertest';
+import app from '../config/app';
+
+describe('Cors', () => {
+    it('should enable cors', async () => {
+        app.get('/test/cors', (req, res) => {
+            res.send();
+        });
+
+        await request(app)
+            .get('/test/cors')
+            .expect('access-control-allow-origin', '*')
+            .expect('access-control-allow-methods', '*')
+            .expect('access-control-allow-headers', '*');
+    });
+});
--- src/Main/config/middlewares.ts
+++ src/Main/config/middlewares.ts
@@ -1,6 +1,8 @@
 import { Express } from 'express';
 import { bodyParser } from '../Middlewares/borderParser';
+import { cors } from '../Middlewares/cors';
 
 export default (app: Express): void => {
     app.use(bodyParser);
+    app.use(cors);
 };
