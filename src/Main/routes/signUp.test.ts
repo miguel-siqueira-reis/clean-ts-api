674aac2812a6f5e2b8422657ad58d01dccc78d7c
miguel-siqueira-reis <miguel.siqueira.reis@gmail.com>
2022-08-01 08:55:05 -0300

    test: integration with mongodb test router singup

--- src/Main/routes/signUp.test.ts
+++ src/Main/routes/signUp.test.ts
@@ -1,8 +1,25 @@
 // eslint-disable-next-line import/no-extraneous-dependencies
 import request from 'supertest';
 import app from '../config/app';
+import { MongoHelper } from '../../Infra/Database/Helpers/MongoHelper';
 
 describe('SignUp', () => {
+    beforeAll(async () => {
+        // eslint-disable-next-line @typescript-eslint/ban-ts-comment
+        // @ts-ignore
+        // eslint-disable-next-line no-underscore-dangle
+        await MongoHelper.connect(process.env.MONGO_URL);
+    });
+
+    afterAll(async () => {
+        await MongoHelper.disconnect();
+    });
+
+    beforeEach(async () => {
+        const accountCollection = MongoHelper.getCollection('accounts');
+        await accountCollection.deleteMany({});
+    });
+
     it('should return an account on success', async () => {
         await request(app)
             .post('/api/signup')
