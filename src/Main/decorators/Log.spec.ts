d59c74ca5cf55d8946d4ccf139d3f1c4d38085ba
miguel-siqueira-reis <miguel.siqueira.reis@gmail.com>
2022-08-15 21:14:39 -0300

    test: ensure LogControllerDecorator Calls Controller with Correct values

---
+++ src/Main/decorators/Log.spec.ts
@@ -0,0 +1,45 @@
+import { LogControllerDecorator } from './Log';
+import {
+    Controller,
+    HttpRequest,
+    HttpResponse,
+} from '../../Presentation/Protocols';
+
+function makeSut() {
+    class ControllerStub implements Controller {
+        // eslint-disable-next-line @typescript-eslint/no-unused-vars
+        async handle(httpRequest: HttpRequest): Promise<HttpResponse> {
+            return {
+                statusCode: 200,
+                body: {
+                    name: 'any_name',
+                },
+            };
+        }
+    }
+
+    const controllerStub = new ControllerStub();
+
+    return {
+        sut: new LogControllerDecorator(controllerStub),
+        controllerStub,
+    };
+}
+
+describe('LogControllerDecorator', () => {
+    it('should call handle method and provide correct params', () => {
+        const { sut, controllerStub } = makeSut();
+        const handleSpy = jest.spyOn(controllerStub, 'handle');
+
+        const httpRequest: HttpRequest = {
+            body: {
+                name: 'any_name',
+                email: 'email@email.com',
+                password: 'any_password',
+                passwordConfirmation: 'any_password',
+            },
+        };
+        sut.handle(httpRequest);
+        expect(handleSpy).toHaveBeenCalledWith(httpRequest);
+    });
+});
