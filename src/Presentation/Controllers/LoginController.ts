a2eee0a0fb4733dd8471b1b262ee343d6da09354
miguel-siqueira-reis <miguel.siqueira.reis@gmail.com>
2022-08-16 07:18:01 -0300

    feat: ensure integration LoginController with emailValidator

--- src/Presentation/Controllers/LoginController.ts
+++ src/Presentation/Controllers/LoginController.ts
@@ -1,12 +1,19 @@
 import { Controller, HttpRequest, HttpResponse } from '../Protocols';
-import { MissingParamError } from '../Errors';
+import { InvalidParamError, MissingParamError } from '../Errors';
 import {
     BadRequest,
     ServerErrorResponse,
     Success,
 } from '../Helpers/HttpHelper';
+import { EmailValidator } from '../Protocols/EmailValidator';
 
 export class LoginController implements Controller {
+    private readonly emailValidator: EmailValidator;
+
+    constructor(emailValidator: EmailValidator) {
+        this.emailValidator = emailValidator;
+    }
+
     async handle(req: HttpRequest): Promise<HttpResponse> {
         try {
             const fieldsRequired = ['email', 'password'];
@@ -14,6 +21,13 @@ export class LoginController implements Controller {
                 if (!req.body[field]) {
                     return BadRequest(new MissingParamError(field));
                 }
+            }
+
+            const { email } = req.body;
+
+            const isValidEmail = this.emailValidator.isValid(email);
+            if (!isValidEmail) {
+                return BadRequest(new InvalidParamError('email'));
             }
 
             return Success({});
