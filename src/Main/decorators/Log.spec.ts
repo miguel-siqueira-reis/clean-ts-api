9badedcc2b97692c3d52a04efac622fe48839b0e
miguel-siqueira-reis <miguel.siqueira.reis@gmail.com>
2022-08-15 21:35:55 -0300

    test: ensure LogControllerDecorator returns the same result of the controller

--- src/Main/decorators/Log.spec.ts
+++ src/Main/decorators/Log.spec.ts
@@ -46,4 +46,23 @@ describe('LogControllerDecorator', () => {
         sut.handle(httpRequest);
         expect(handleSpy).toHaveBeenCalledWith(httpRequest);
     });
+
+    it('should return the same result of the controller', async () => {
+        const { sut } = makeSut();
+        const httpRequest: HttpRequest = {
+            body: {
+                name: 'any_name',
+                email: 'email@mail.com',
+                password: 'any_password',
+                passwordConfirmation: 'any_password',
+            },
+        };
+        const httpResponse = await sut.handle(httpRequest);
+        expect(httpResponse).toEqual({
+            statusCode: 200,
+            body: {
+                name: 'any_name',
+            },
+        });
+    });
 });
