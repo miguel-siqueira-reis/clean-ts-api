b757a3d08b94e02e30b95a8563ea9d46c3b0ab02
miguel-siqueira-reis <miguel.siqueira.reis@gmail.com>
2022-08-17 09:55:58 -0300

    test: ensure RequiredFieldValidatin not returns a MissingParam erro if no fails validation

--- src/Presentation/Helpers/Validators/RequiredFieldValidation.spec.ts
+++ src/Presentation/Helpers/Validators/RequiredFieldValidation.spec.ts
@@ -7,4 +7,10 @@ describe('RequiredFieldValidation', () => {
         const error = sut.validate({ other_field: 'any_value' });
         expect(error).toEqual(new MissingParamError('any_field'));
     });
+
+    it('should not return missing params errror if validate no fails', () => {
+        const sut = new RequiredFieldValidation('field');
+        const error = sut.validate({ field: 'any_value' });
+        expect(error).toBeFalsy();
+    });
 });
