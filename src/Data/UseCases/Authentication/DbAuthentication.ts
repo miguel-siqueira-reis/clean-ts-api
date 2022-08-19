c501313f10c98403364d1db6595cc9697f849f85
miguel-siqueira-reis <miguel.siqueira.reis@gmail.com>
2022-08-19 13:23:00 -0300

    feat: ensure dbAuthentication class HashComparer with correct values

--- src/Data/UseCases/Authentication/DbAuthentication.ts
+++ src/Data/UseCases/Authentication/DbAuthentication.ts
@@ -3,14 +3,22 @@ import {
     AuthenticationData,
 } from '../../../Domain/useCases/Authentication';
 import { LoadAccountByEmailRepository } from '../../Protocols/Database/LoadAccountByEmailRepository';
+import { HashComparer } from '../../Protocols/Criptography/HashComparer';
 
 export class DbAuthentication implements Authentication {
     constructor(
         private readonly loadAccountByEmailRepository: LoadAccountByEmailRepository,
+        private readonly hashComparer: HashComparer,
     ) {}
 
-    async auth({ email }: AuthenticationData): Promise<string | null> {
-        await this.loadAccountByEmailRepository.load(email);
+    async auth({
+        email,
+        password,
+    }: AuthenticationData): Promise<string | null> {
+        const account = await this.loadAccountByEmailRepository.load(email);
+        if (account) {
+            await this.hashComparer.compare(password, account.password);
+        }
         return null;
     }
 }
