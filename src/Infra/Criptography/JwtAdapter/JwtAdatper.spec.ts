c6bc66fd7ce87fb3222466d12efd9d49701ce5d7
miguel-siqueira-reis <miguel.siqueira.reis@gmail.com>
2022-08-21 15:49:11 -0300

    test: ensure JwtAdapter return throw if sign throw

--- src/Infra/Criptography/JwtAdapter/JwtAdatper.spec.ts
+++ src/Infra/Criptography/JwtAdapter/JwtAdatper.spec.ts
@@ -11,14 +11,23 @@ describe('JwtAdapter', () => {
         const signSpy = jest.spyOn(jwt, 'sign');
 
         await sut.encrypt('any_id');
-
         expect(signSpy).toHaveBeenCalledWith({ id: 'any_id' }, 'secret');
     });
 
     it('should return token on success', async () => {
         const sut = new JwtAdatper('secret');
-        const token = await sut.encrypt('any_id');
 
+        const token = await sut.encrypt('any_id');
         expect(token).toBe('any_token');
+    });
+
+    it('should throw if sign throws', async () => {
+        const sut = new JwtAdatper('secret');
+        jest.spyOn(jwt, 'sign').mockImplementationOnce(() => {
+            throw new Error();
+        });
+
+        const promise = sut.encrypt('any_id');
+        await expect(promise).rejects.toThrow();
     });
 });
