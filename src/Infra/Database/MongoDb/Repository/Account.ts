b9169d5842da363ac88b2e8ed38e73bb91f41677
miguel-siqueira-reis <miguel.siqueira.reis@gmail.com>
2022-08-21 16:43:01 -0300

    feat: ensure Account loadByEmail return account on success

--- src/Infra/Database/MongoDb/Repository/Account.ts
+++ src/Infra/Database/MongoDb/Repository/Account.ts
@@ -2,8 +2,11 @@ import { AddAccountRepository } from '../../../../Data/Protocols/Database/AddAcc
 import { AddAccountModel } from '../../../../Domain/useCases/AddAccount';
 import { AccountModel } from '../../../../Domain/Models/Account';
 import { MongoHelper } from '../Helpers/MongoHelper';
+import { LoadAccountByEmailRepository } from '../../../../Data/Protocols/Database/LoadAccountByEmailRepository';
 
-export class AccountRepository implements AddAccountRepository {
+export class AccountRepository
+    implements AddAccountRepository, LoadAccountByEmailRepository
+{
     async add(accountData: AddAccountModel): Promise<AccountModel> {
         const accountCollection = await MongoHelper.getCollection('accounts');
         const result = await accountCollection.insertOne(accountData);
@@ -11,5 +14,22 @@ export class AccountRepository implements AddAccountRepository {
             id: result.insertedId.toString(),
             ...accountData,
         };
+    }
+
+    async loadByEmail(email: string): Promise<AccountModel | null> {
+        const accountCollection = await MongoHelper.getCollection('accounts');
+        const accountData = await accountCollection.findOne({
+            email,
+        });
+
+        return (
+            accountData && {
+                // eslint-disable-next-line no-underscore-dangle
+                id: accountData._id.toString(),
+                name: String(accountData.name),
+                email: String(accountData.email),
+                password: String(accountData.password),
+            }
+        );
     }
 }
