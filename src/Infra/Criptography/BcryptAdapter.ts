4e70168975aa3f084263c07b27640680713c904d
miguel-siqueira-reis <miguel.siqueira.reis@gmail.com>
2022-08-21 08:47:41 -0300

    feat: ensure BryptAdapter call compare with correct values

--- src/Infra/Criptography/BcryptAdapter.ts
+++ src/Infra/Criptography/BcryptAdapter.ts
@@ -1,7 +1,8 @@
 import bcrypt from 'bcrypt';
 import { Hasher } from '../../Data/Protocols/Criptography/Hasher';
+import { HashComparer } from '../../Data/Protocols/Criptography/HashComparer';
 
-export class BcryptAdapter implements Hasher {
+export class BcryptAdapter implements Hasher, HashComparer {
     private readonly salt: number;
 
     constructor(salt: number) {
@@ -10,5 +11,9 @@ export class BcryptAdapter implements Hasher {
 
     async hash(password: string): Promise<string> {
         return await bcrypt.hash(password, this.salt);
+    }
+
+    async compare(value: string, hash: string): Promise<boolean> {
+        return await bcrypt.compare(value, hash);
     }
 }
