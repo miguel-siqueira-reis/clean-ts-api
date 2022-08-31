42ceb2e415122be31f4cb4b977b45ab5a6696e06
miguel-siqueira-reis <miguel.siqueira.reis@gmail.com>
2022-08-31 17:01:21 -0300

    chore: chenge mongoDb localhost to mongodb Atlas

--- src/Main/config/db.ts
+++ src/Main/config/db.ts
@@ -1,9 +1,11 @@
 import { MongoHelper } from '../../Infra/Database/MongoDb/Helpers/MongoHelper';
 
 export default async function initDb() {
-    const { MONGODB_HOST, MONGODB_PORT, MONGODB_DB } = process.env;
+    const { MONGO_USER, MONGO_PASSWORD, MONGODB_CLUSTER, MONGODB_DB } =
+        process.env;
+
     await MongoHelper.connect(
-        `mongodb://${MONGODB_HOST}:${MONGODB_PORT}/${MONGODB_DB}`,
+        `mongodb+srv://${MONGO_USER}:${MONGO_PASSWORD}@${MONGODB_CLUSTER}.gmwdppo.mongodb.net/${MONGODB_DB}?retryWrites=true&w=majority`,
         // eslint-disable-next-line no-console
     );
 }
