6ec5f6e5a2bea96be2c289b248568d688a0ab5d4
miguel-siqueira-reis <miguel.siqueira.reis@gmail.com>
2022-10-20 04:07:40 -0300

    refactor: add .env in .gitignore add new file in project .env.example and define url connection db with mode variable in .env

--- src/Main/config/db.ts
+++ src/Main/config/db.ts
@@ -1,11 +1,21 @@
 import { MongoHelper } from '../../Infra/Database/MongoDb/Helpers/MongoHelper';
 
 export default async function initDb() {
-    const { MONGO_USER, MONGO_PASSWORD, MONGODB_CLUSTER, MONGODB_DB } =
-        process.env;
+    const {
+        MONGO_USER,
+        MONGO_PASSWORD,
+        MONGODB_CLUSTER,
+        MONGODB_DB,
+        MONGO_MODE,
+    } = process.env;
 
-    await MongoHelper.connect(
-        `mongodb+srv://${MONGO_USER}:${MONGO_PASSWORD}@${MONGODB_CLUSTER}.gmwdppo.mongodb.net/${MONGODB_DB}?retryWrites=true&w=majority`,
-        // eslint-disable-next-line no-console
-    );
+    let mongoUrl;
+
+    if (MONGO_MODE === 'production') {
+        mongoUrl = `mongodb+srv://${MONGO_USER}:${MONGO_PASSWORD}@${MONGODB_CLUSTER}.gmwdppo.mongodb.net/${MONGODB_DB}?retryWrites=true&w=majority`;
+    } else {
+        mongoUrl = `mongodb://mongo:27017/clean-node-api`;
+    }
+
+    await MongoHelper.connect(mongoUrl);
 }
--- src/Main/routes/login.test.ts
+++ src/Main/routes/login.test.ts
@@ -1,3 +1,5 @@
+// eslint-disable-next-line @typescript-eslint/ban-ts-comment
+// @ts-ignore
 // eslint-disable-next-line import/no-extraneous-dependencies
 import request from 'supertest';
 import 'dotenv/config';
